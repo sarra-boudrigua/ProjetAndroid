cbef7138c14ad8e94f4e27e242461fd09cd5e272
sboudrig <sarasousse7@gmail.com>
2024-11-13 14:45:42 +0100

    Noms des collections OK!!!

--- app/src/main/java/com/example/tp1/Examen.kt
+++ app/src/main/java/com/example/tp1/Examen.kt
@@ -1,8 +1,24 @@
 package com.example.tp1
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
-fun examenScreen (){
+fun examenScreen (viewModel: MainViewModel = viewModel()){
+    val collection by viewModel.collection.collectAsState()
+    if (collection.isEmpty()) {
+        viewModel.searchCollection()
+    }
+    LazyColumn {
+        items (collection) { c ->
+            Text(text = c.name)
+        }
+    }
 
 }
